cba2e116a3095a7b4ba4610bd0cd237e1114fdf4
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2021-06-04 10:43:56 +0300

    Updated proto files

--- src/main/java/com/example/demo/NotificationServiceApplication.java
+++ src/main/java/com/example/demo/NotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import skysoft.enterprise.backend.bots.BotsGrpc;
@@ -17,68 +18,78 @@ public class NotificationServiceApplication {
 
     private static final Scanner scanner = new Scanner(System.in);
     public static final String chatId = "581548594";
+//    public static final String chatId = "581548595"; //wrong
 //    public static final String chatId = "524376687"; //tanya
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationServiceApplication.class, args);
+        try {
+            SpringApplication.run(NotificationServiceApplication.class, args);
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8081)
-                .usePlaintext() // TODO: 28.04.21 don`t use it
-                .build();
+            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8081)
+                    .usePlaintext() // TODO: 28.04.21 don`t use it
+                    .build();
 
-        BotsGrpc.BotsBlockingStub stub = BotsGrpc.newBlockingStub(channel);
-        System.out.println("Open channel");
+            BotsGrpc.BotsBlockingStub stub = BotsGrpc.newBlockingStub(channel);
+            System.out.println("Open channel");
 
-        while (true) {
-            System.out.println("Choose scenario Mfa or Notification (enter \"0\" or \"1\") ");
-            int scenarioId = scanner.nextInt();
-            switch (scenarioId) {
-                case 0: {
-                    System.out.println("Input OTP: ");
-                    int otp = scanner.nextInt();
+            while (true) {
+                System.out.println("Choose scenario Mfa or Notification (enter \"0\" or \"1\") ");
+                int scenarioId = scanner.nextInt();
+                switch (scenarioId) {
+                    case 0: {
+                        System.out.println("Input OTP: ");
+                        int otp = scanner.nextInt();
 
-                    BotsOuterClass.Scenario scenario = BotsOuterClass.Scenario.newBuilder()
-                            .setId(UUID.randomUUID().toString())
-                            .setType(BotsOuterClass.ScenarioType.Mfa)
-                            .setData(BotsOuterClass.ScenarioData.newBuilder()
-                                    .setMessage("Confirm authorisation in Time tracking on " + Date.from(Instant.now()))
-                                    .putParameters("chatId", chatId)
-                                    .putParameters("otp", String.valueOf(otp))
-                                    .build())
-                            .setBotType(BotsOuterClass.BotType.Telegram)
-                            .build();
-                    stub.runScenario(scenario);
+                        BotsOuterClass.Scenario scenario = BotsOuterClass.Scenario.newBuilder()
+                                .setId(UUID.randomUUID().toString())
+                                .setType(BotsOuterClass.ScenarioType.MfaScenario)
+                                .setUserIdentifier(chatId)
+                                .setData(BotsOuterClass.ScenarioData.newBuilder()
+                                        .setMessage("Confirm authorisation in Time tracking on " + Date.from(Instant.now()))
+                                        .putParameters("otp", String.valueOf(otp))
+                                        .build())
+                                .setBotType(BotsOuterClass.BotType.TelegramBot)
+                                .build();
+                        stub.runScenario(scenario);
+                        break;
+                    }
+                    case 1: {
+                        System.out.println("Input message: ");
+                        String message = scanner.next();
+
+                        BotsOuterClass.Scenario scenario = BotsOuterClass.Scenario.newBuilder()
+                                .setId(UUID.randomUUID().toString())
+                                .setType(BotsOuterClass.ScenarioType.NotificationScenario)
+                                .setUserIdentifier(chatId)
+                                .setData(BotsOuterClass.ScenarioData.newBuilder()
+                                        .setMessage(message)
+                                        .build())
+                                .setBotType(BotsOuterClass.BotType.TelegramBot)
+                                .build();
+                        stub.runScenario(scenario);
+                        break;
+                    }
+
+                    case 2:
+                    default:
+                }
+
+                System.out.println("Input command \"c\" for continue work or \"t\" for close channel");
+                String command = scanner.next();
+
+                if (command.equals("t")) {
+                    channel.shutdownNow();
+                    System.out.println("Channel closed");
                     break;
                 }
-                case 1: {
-                    System.out.println("Input message: ");
-                    String message = scanner.next();
-
-                    BotsOuterClass.Scenario scenario = BotsOuterClass.Scenario.newBuilder()
-                            .setId(UUID.randomUUID().toString())
-                            .setType(BotsOuterClass.ScenarioType.Notification)
-                            .setData(BotsOuterClass.ScenarioData.newBuilder()
-                                    .setMessage(message)
-                                    .putParameters("chatId", chatId)
-                                    .build())
-                            .setBotType(BotsOuterClass.BotType.Telegram)
-                            .build();
-                    stub.runScenario(scenario);
-                    break;
-                }
-
-                case 2:
-                default:
             }
-
-            System.out.println("Input command \"c\" for continue work or \"t\" for close channel");
-            String command = scanner.next();
-
-            if (command.equals("t")) {
-                channel.shutdownNow();
-                System.out.println("Channel closed");
-                break;
-            }
+        } catch (StatusRuntimeException e) {
+            System.out.println("Status: " + e.getStatus());
+            System.out.println("Code: " + e.getStatus().getCode());
+            System.out.println("Desc: " + e.getStatus().getDescription());
+            System.out.println(e.getTrailers().toString());
+            System.out.println();
+            e.printStackTrace();
         }
     }
 }
